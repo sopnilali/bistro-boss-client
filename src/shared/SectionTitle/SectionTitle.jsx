aa6e7916793fbc611f8437caeda41a5825764d09
Md Abdul Adud <sopnilali24@gmail.com>
2024-10-19 21:23:36 +0600

    addeD

--- src/shared/SectionTitle/SectionTitle.jsx
+++ src/shared/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SectionTitle = ({subtitle, Title}) => {
     return (
         <div className='w-[400px] mx-auto text-center'>
-            <p className='italic text-yellow-700'>---{subtitle}---</p>
+            <p className='italic text-yellow-700'>--{subtitle}--</p>
             <div className='divider'></div>
             <h3 className='uppercase text-[40px]'>{Title}</h3>
             <div className='divider'></div>
