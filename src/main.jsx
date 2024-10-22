591a3245b1bc0d56e658b2fdc3691ad6d8ca5b59
Md Abdul Adud <sopnilali24@gmail.com>
2024-10-22 22:44:13 +0600

    added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import AuthProvider from './provider/Authprovider'
+import AuthProvider from './provider/AuthProvider'
 import Routes from './routes/Routes'
 
 createRoot(document.getElementById('root')).render(
